efdb35e5c87fa76c007e48b47c196689e6a00720
nyholzer <nissan.holzer@gmail.com>
2019-08-20 00:15:22 -0400

    Done.

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ function nestedTarget(){
 }
 
 function deepestChild() {
-    const lis = document.querySelector('#grand-node');
-    let ch = ''
-    for (let i = 0; i < lis.length; i++) {
-        ch = lis[i].innerHTML = (i + 1).toString();
-        return ch
+    let lis = document.getElementById("grand-node").querySelectorAll("div");
+    let test;
+    for(let i=0; i < lis.length-1; i++) {
+      test=lis[i].querySelector("div");
     }
-}
+    return test;
+  }
+
+  function increaseRankBy(n) {
+    const list = document.querySelectorAll('ul.ranked-list li')
+    for (var i = 0; i < list.length; i++) {
+      list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+    }
+  }
